3629d0abf14d98de9ae849918211f98e9d2d6eab
EdgarRamosLuna <edgarramoslun333@gmail.com>
2023-06-02 14:55:25 -0600

    menus

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,27 +1,30 @@
+import { Link } from "react-router-dom";
 import "./menu.scss"
 import React from 'react';
 
 
 export default function Menu({ menuOpen, setMenuOpen }) {
-  return (
-    <div className={"menu "+(menuOpen && "active")}>
-        <ul>
-            <li onClick={() =>setMenuOpen(false)}>
-                <a href="/">Inicio</a>
-            </li>
-            <li onClick={() =>setMenuOpen(false)}>
-                <a href="/#autos">Modelos</a>
-            </li>
-            <li onClick={() =>setMenuOpen(false)}>
-                <a href="/#comunidad">¿Como funciona?</a>
-            </li>
-            <li onClick={() =>setMenuOpen(false)}>
-                <a href="/#beneficios">Beneficios</a>
-            </li>
-            <li onClick={() =>setMenuOpen(false)}>
-                <a href="/#contacto">Contacto</a>
-            </li>
-        </ul>
-    </div>
-  )
+    return (
+        <div className={"menu " + (menuOpen && "active")}>
+            <ul>
+                <li onClick={() => setMenuOpen(false)}>
+                    <Link to="/" className="logo">
+                        Inicio
+                    </Link>
+                </li>
+                <li onClick={() => setMenuOpen(false)}>
+                    <a href="/#autos">Modelos</a>
+                </li>
+                <li onClick={() => setMenuOpen(false)}>
+                    <a href="/#comunidad">¿Como funciona?</a>
+                </li>
+                <li onClick={() => setMenuOpen(false)}>
+                    <a href="/#beneficios">Beneficios</a>
+                </li>
+                <li onClick={() => setMenuOpen(false)}>
+                    <Link to="/vacante">Únete al equipo</Link>
+                </li>
+            </ul>
+        </div>
+    )
 }
